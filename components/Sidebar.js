9c7851c3bd712d7b4f2b1ba41bb0f5ac0b8411d4
Armando Ruiz <armando.ruiz@industrie.co>
2023-03-30 10:41:00 +1100

    374. Obtener las categorias de la API,

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import useQuiosco from "../hooks/useQuiosco";
+import Categoria from "./Categoria";
 
 const Sidebar = () => {
+  const { categorias } = useQuiosco()
   return (
     <>
       <Image
@@ -9,6 +12,14 @@ const Sidebar = () => {
         src="/assets/img/logo.svg"
         alt="imagen logotipo"
       />
+      <nav className="mt-10">
+        {categorias.map(categoria => (
+          <Categoria
+            key={categoria.id}
+            categoria={categoria}
+          />
+        ))}
+      </nav>
     </>
   );
 };
