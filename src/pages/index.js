45abe91c5718d7862b230022da1f160c3226ba0d
Armando Ruiz <armando.ruiz@industrie.co>
2023-03-30 09:14:01 +1100

    371. creando el layout principal

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,28 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Layout from '../../layout/Layout'
 import { Inter } from 'next/font/google'
 import { PrismaClient } from '@prisma/client'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home({categorias}) {
-  console.log(categorias)
+export default function Home() {
+  // console.log(categorias)
   return (
-    <>
+    <Layout>
       <h1>Hola Mundo!</h1>
-    </>
+    </Layout>
   )
 }
 
-export const getServerSideProps = async () => {
-  const prisma = new PrismaClient()
+// export const getServerSideProps = async () => {
+//   const prisma = new PrismaClient()
 
-  const categorias = await prisma.categoria.findMany()
-  // console.log(categorias)
-  return {
-    props: {
-      categorias,
-    }
-  }
-}
+//   const categorias = await prisma.categoria.findMany()
+//   // console.log(categorias)
+//   return {
+//     props: {
+//       categorias,
+//     }
+//   }
+// }
