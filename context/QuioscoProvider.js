14945cd7695555c232836dd41519506b5951aee9
Armando Ruiz <armando.ruiz@industrie.co>
2023-03-30 11:04:08 +1100

    375. Detectando la categoria Actual

--- context/QuioscoProvider.js
+++ context/QuioscoProvider.js
@@ -5,6 +5,7 @@ const QuioscoContext = createContext()
 
 const QuioscoProvider = ({children}) => {
   const [categorias, setCategorias] = useState([])
+  const [categoriaActual, setCategoriaActual] = useState({})
 
   const obtenerCategorias = async () => {
     const { data } = await axios('/api/categorias')
@@ -15,10 +16,20 @@ const QuioscoProvider = ({children}) => {
     obtenerCategorias()
   },[])
 
+  const handleClickCategoria = id => {
+    // console.log(id)
+    const categoria = categorias.filter(cat => cat.id === id)
+    // console.log(categoria) imprime un arreglo
+    setCategoriaActual(categoria[0])
+
+  }
+
   return(
     <QuioscoContext.Provider
       value={{
-        categorias
+        categorias,
+        categoriaActual,
+        handleClickCategoria
       }}
     >
       {children}
