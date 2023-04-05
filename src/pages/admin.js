80ed9cad5fe4278cd5f9bd1cc92141f8acdd465d
Armando Ruiz <armando.ruiz@industrie.co>
2023-04-05 14:37:02 +1000

    408. Consultando el endpoint de Ordenes (la API) con e hook useSWR

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,6 +1,16 @@
+
+import useSWR from 'swr'
+import axios from 'axios'
 import AdminLayout from "../../layout/AdminLayout"
 
 export default function Admin() {
+  const fetcher = () => axios('/api/ordenes').then(datos => datos.data)
+  const { data, error, isLoading } = useSWR('/api/ordenes', fetcher)
+  console.log("admin")
+  console.log(data)
+  console.log(error)
+  console.log(isLoading)
+
 
   return(
     <AdminLayout pagina={"Admin"}>
