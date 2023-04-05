ee22bef7c5ae72395658d60eaba01a0b1ad74a05
Armando Ruiz <armando.ruiz@industrie.co>
2023-04-05 14:45:11 +1000

    409. Primeros pasos para mostrar las ordenes

---
+++ components/Orden.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Orden = ({orden}) => {
+  // console.log(orden)
+  const { id, nombre, total, pedido } = orden
+
+  return (
+    <div className="border p-10 space-y-5">
+      <h3 className="text-2xl font-bold">Orden: {id}</h3>
+      <p className="text-lg font-bold">Cliente: {nombre}</p>
+
+    </div>
+  );
+};
+
+export default Orden;
