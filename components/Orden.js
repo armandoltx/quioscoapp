298a5de744411796a6a5d497eb591a8be4f73a91
Armando Ruiz <armando.ruiz@industrie.co>
2023-04-05 15:01:15 +1000

    411. Creando el boton para completar las ordenes

--- components/Orden.js
+++ components/Orden.js
@@ -6,6 +6,10 @@ const Orden = ({orden}) => {
   // console.log(orden)
   const { id, nombre, total, pedido } = orden
 
+  const completarOrden = () => {
+    console.log("completando...", id)
+  }
+
   return (
     <div className="border p-10 space-y-5">
       <h3 className="text-2xl font-bold">Orden: {id}</h3>
@@ -32,6 +36,13 @@ const Orden = ({orden}) => {
         <p className="mt-5 font-black text-4xl text-amber-500">
           Total a Pagar: {formatearDinero(total)}
         </p>
+        <button
+          className="bg-indigo-600 hover:bg-indigo-800 text-white mt-5 md:mt-0 py-3 px-10 uppercase font-bold rounded-lg"
+          type="button"
+          onClick={completarOrden}
+        >
+          Completar Orden
+        </button>
       </div>
     </div>
   );
